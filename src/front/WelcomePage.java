726e91230f8b146d104f2aa6220acb19fca4ac2d
ElizabethGhreir <elzighreir@gmail.com>
2024-02-06 22:51:54 -0500

    Revert "modified LoginPage and added unit tests"
    This reverts commit 2bca61f0a83e42b1f474a38fdb33590070629eb7.

--- src/front/WelcomePage.java
+++ src/front/WelcomePage.java
@@ -4,22 +4,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class WelcomePage extends JPanel {
-	public WelcomePage(LoginPage loginPage) {
-		setBackground(new Color(45,45,45)); // Set the background colour
+    public WelcomePage(LoginPage loginPage) {
+        setBackground(new Color(45,45,45)); // Set the background colour
 
-		JLabel welcomeLabel = new JLabel("Welcome to the new page!");
-		welcomeLabel.setForeground(Color.WHITE);
-		add(welcomeLabel);
+        JLabel welcomeLabel = new JLabel("Welcome to the new page!");
+        welcomeLabel.setForeground(Color.WHITE);
+        add(welcomeLabel);
 
-		JButton signOutButton = new JButton("Sign Out");
-		add(signOutButton);
+        JButton signOutButton = new JButton("Sign Out");
+        add(signOutButton);
 
-		signOutButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Call the signOut method in LoginPage to switch to the login page
-				loginPage.signOut();
-			}
-		});
-	}
+        signOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Call the signOut method in LoginPage to switch to the login page
+                loginPage.signOut();
+            }
+        });
+    }
 }
