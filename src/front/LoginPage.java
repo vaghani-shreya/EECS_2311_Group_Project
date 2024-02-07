0f11de9765444871de7e260cf304ab68bb9ddc21
Shreya Vaghani <shreyavaghani6123@gmail.com>
2024-02-07 18:55:24 -0500

    working login page and dashboard

--- src/front/LoginPage.java
+++ src/front/LoginPage.java
@@ -1,7 +1,10 @@
 package front;
 import javax.swing.*;
+
+
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Window.Type;
 
 public class LoginPage extends JFrame {
     private JTextField usernameField;
@@ -9,8 +12,10 @@ public class LoginPage extends JFrame {
     private JPanel cardPanel;
     private CardLayout cardLayout;
 
+
     public LoginPage() {
-        setTitle("Login");
+    	
+        setTitle("Show Tracking Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Create components
@@ -23,7 +28,7 @@ public class LoginPage extends JFrame {
 
         // Label Properties
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(new Font(titleLabel.getFont().getName(), Font.PLAIN, 24));
+        titleLabel.setFont(new Font(titleLabel.getFont().getName(), Font.PLAIN, 30));
         usernameLabel.setForeground(Color.WHITE); 
         passwordLabel.setForeground(Color.WHITE);
 
@@ -59,10 +64,14 @@ public class LoginPage extends JFrame {
 
         // Create welcome panel
         WelcomePage welcomePanel = new WelcomePage(this);
+        dashBoard dbPanel = new dashBoard(this);
 
         // Add panels to cardPanel
         cardPanel.add(contentPane, "login");
         cardPanel.add(welcomePanel, "welcome");
+        cardPanel.add(dbPanel, "dashBoard");
+        
+        
 
         // Set contentPane to cardPanel
         setContentPane(cardPanel);
@@ -81,7 +90,13 @@ public class LoginPage extends JFrame {
 
                 // For simplicity, let's use a hardcoded username and password for demonstration
                 if (username.equals("user") && enteredPassword.equals("password")) {
-                    cardLayout.show(cardPanel, "welcome");
+//                	JFrame frame = dashBoard.getInstance();
+//                	frame.setSize(900, 600);
+//                	frame.pack();
+//                	frame.setVisible(true);
+                	
+                	cardLayout.show(cardPanel, "dashBoard");
+                  //  cardLayout.show(cardPanel, "welcome");
                 } else {
                     JOptionPane.showMessageDialog(LoginPage.this, "Login failed. Please try again.");
                 }
@@ -98,7 +113,10 @@ public class LoginPage extends JFrame {
     }
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
-            new LoginPage().setVisible(true);
+        	LoginPage loginPage = new LoginPage();
+            loginPage.setExtendedState(JFrame.MAXIMIZED_BOTH); // Set the frame to full size
+            loginPage.setVisible(true);
+            //new LoginPage().setVisible(true);
         });
     }
 }
