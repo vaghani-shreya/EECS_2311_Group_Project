f7e60cbfa90f101f0d501b34e00c4488c7c221ed
ElizabethGhreir <elzighreir@gmail.com>
2024-03-22 17:37:59 -0400

    create new account implementation

--- src/recommendation/TMDBFetcher.java
+++ src/recommendation/TMDBFetcher.java
@@ -12,8 +12,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TMDBFetcher {
-	//private static final String API_KEY = "your_api_key_here"; // Use TMDb API
-	private static final String API_KEY = "a4ef5941510a12f6514183ed76fd459a"; // Remove this when pushing
+	private static final String API_KEY = "your_api_key_here"; // Use TMDb API
     private static final String BASE_URL = "https://api.themoviedb.org/3"; // TMDb API URL
     private static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // width 500
 
