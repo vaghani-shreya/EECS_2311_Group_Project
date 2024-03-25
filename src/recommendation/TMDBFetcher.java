7d69783465a662f5027b7cd4e72711db3407214a
ElizabethGhreir <elzighreir@gmail.com>
2024-03-24 21:04:43 -0400

    saving merged changes between Anusha and David

--- src/recommendation/TMDBFetcher.java
+++ src/recommendation/TMDBFetcher.java
@@ -15,8 +15,7 @@ public class TMDBFetcher {
 
 	//private static final String API_KEY = "your_api_key_here"; // Use TMDb API
 
-     private static final String API_KEY = "your_api_key_here"; // Use TMDb API
-	//private static final String API_KEY = "0823bff25f354a2c90dd07428e03c69e"; // Remove this when pushing
+	private static final String API_KEY = "your_api_key_here"; // Use TMDb API
     private static final String BASE_URL = "https://api.themoviedb.org/3"; // TMDb API URL
     private static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // width 500
 
